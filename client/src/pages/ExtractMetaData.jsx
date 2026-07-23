3daf4e3a5ee7030ca16671d424c72197aecd9236
r-a-q <156955463+r-a-q@users.noreply.github.com>
2026-07-22 19:32:14 -0700

    chore: testing gallery card format

---
+++ client/src/pages/ExtractMetaData.jsx
@@ -0,0 +1,17 @@
+import {useEffect} from "react";
+
+//https://api.artic.edu/api/v1/artworks/search?
+//fields=
+//limit=
+//
+
+function ExtractMetaData() {
+
+    useEffect(() => {
+        
+
+    }, [])
+
+}
+
+export default ExtractMetaData;
